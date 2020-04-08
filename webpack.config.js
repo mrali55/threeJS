8f8cb3d49109b0bbf78dee464da7e84fe69676e5
Mustafa <mustafaa@wix.com>
2020-04-09 02:21:09 +0300

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    //mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'src/dist'),
     },
-    watch: true,
+    /*watch: true,
     watchOptions: {
         aggregateTimeout: 600,
-        ignored: ['node_modules/**']
-    }
+        ignored: ['node_modules/!**']
+    }*/
 };
